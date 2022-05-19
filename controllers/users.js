50f23496cc99e3a59d25b58f612bcdd5213deed2
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2022-05-19 11:24:03 +0300

    Проектная работа 13 - в процессе работы

--- controllers/users.js
+++ controllers/users.js
@@ -4,13 +4,22 @@ const user = require('../models/user');
 
 const getUsers = (req, res) => {
   user.find({})
-    .then(users => res.send({data: users}))
+    .then(users => res.status(200).send({data: users}))
   //const userId = req.param.userId
   // const user = users.find(users.userId === +userId)
-  if (!user) {
-    return res.status(404).send({message: 'Пользователь не найден'})
+    .catch(()=> {
+      if (!user) {
+        return res.status(404).send({message: 'Пользователь не найден'})
+      }
+      return res.status(500).send({message: 'Ошибка сервера'})
+    })
   }
-}
+
+
+
+
+
+
 
 const createUser = (req, res) => {
   const {name, about, avatar} = req.body
@@ -54,16 +63,4 @@ module.exports = {
   patchAvatar
 }
 
-/* fs.readFile(path.resolve(__dirname,'..','users-data.json'),'utf-8')
-   .then(fileContent =>{
-     const users = JSON.parse(fileContent)
-     users.push({
-       userId: users.length,
-       name,
-       about,
-       avatar
-     })
-     return users
-   }).then(users => fs.writeFile(path.resolve(__dirname,'..','users-data.json'),JSON.stringify(users)))
-   .then(()=>res.status(201).send({message: 'Пользователь успешно создан'}))
-   .catch(()=>res.status(500).send({message: 'ошибка сервера'}))*/
+
