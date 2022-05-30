146f811d5fff707bb2f43f0a98e7711ad1abdcdb
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2022-05-30 22:54:20 +0300

    Проектная работа 14- в процессе тестирования

--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,38 @@ const router = require('express').Router();
 const {
   getUser, patchUser, patchAvatar, getUsers, getUserMe,
 } = require('../controllers/users');
+const { celebrate, Joi } = require('celebrate');
 
-router.get('/users/me', getUserMe);
-router.get('/users/:userId', getUser);
-router.patch('/users/me', patchUser);
-router.get('/users', getUsers);
-router.patch('/users/me/avatar', patchAvatar);
+router.get('/users/me',celebrate({
+  body: Joi.object().keys({
+    title: Joi.string().required().min(2).max(30),
+    text: Joi.string().required().min(2),
+  }),
+}), getUserMe);
+router.get('/users/:userId',celebrate({
+  body: Joi.object().keys({
+    title: Joi.string().required().min(2).max(30),
+    text: Joi.string().required().min(2),
+  }),
+}), getUser);
+router.patch('/users/me',celebrate({
+  body: Joi.object().keys({
+    title: Joi.string().required().min(2).max(30),
+    text: Joi.string().required().min(2),
+  }),
+}),
+patchUser);
+router.get('/users',celebrate({
+  body: Joi.object().keys({
+    title: Joi.string().required().min(2).max(30),
+    text: Joi.string().required().min(2),
+  }),
+}), getUsers);
+router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    title: Joi.string().required().min(2).max(30),
+    text: Joi.string().required().min(2),
+  }),
+}),  patchAvatar);
 
 module.exports.userRouter = router;
