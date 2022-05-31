a08b4a9a2f04f27e6de81d3143bf6418b95e2364
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2022-05-31 09:56:40 +0300

    Проектная работа 14- в процессе тестирования

--- app.js
+++ app.js
@@ -13,8 +13,11 @@ app.use(express.json());
 app.post('/signin', login);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
-    title: Joi.string().required().min(2).max(30),
-    text: Joi.string().required().min(2),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(5),
+    name: Joi.string().required().min(2).max(30),
+    avatar: Joi.number().integer().required().min(18),
+    about: Joi.string().min(2).max(30),
   }),
 }), createUser);
 app.use('/', auth, userRouter);
