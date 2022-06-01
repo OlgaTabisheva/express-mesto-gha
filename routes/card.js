6885d16c8d50df0b6557a9416a8fb7b34ba5a9fd
OlgaTabisheva <93075832+OlgaTabisheva@users.noreply.github.com>
2022-06-01 19:08:17 +0300

    Проектная работа 14- в процессе тестирования

--- routes/card.js
+++ routes/card.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards, createCards, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
+
 router.get('/cards', getCards);
-router.post('/cards', createCards);
+router.post('/cards',celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.link().required(),
+
+  }),
+}), createCards);
 router.delete('/cards/:cardId', deleteCard);
 router.put('/cards/:cardId/likes', likeCard);
 router.delete('/cards/:cardId/likes', dislikeCard);
